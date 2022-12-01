51dfa3862b059bdfea98a39cdcc9e9700ccfd08a
Ewa <ewni@startupcentral.dk>
2022-12-01 09:50:14 +0100

    tailwind fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+//inside this App.jsx, we import every component, that is implemented separately one by one
 import React from 'react'
 
-const App = () => {
-  return (
+const App = () => (
     //this div wraps whole application
     <div className="bg-primary w-full overflow-hidden">  
-        <h1>Hello, World!</h1>
+        <h1>Ewa</h1>
     </div>
-  )
-}
+  );
 
 export default App
