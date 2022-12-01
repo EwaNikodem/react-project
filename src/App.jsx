dd0f4a8404a04229cc6a16b5f1975392f353bf76
Ewa <ewni@startupcentral.dk>
2022-12-01 14:53:06 +0100

    adding divs elements and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 //inside this App.jsx, we import every component, that is implemented separately one by one
-import React from 'react'
+import styles from './style';
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => (
     //this div wraps whole application
     <div className="bg-primary w-full overflow-hidden">  
-        <h1>Ewa</h1>
+    {/* div below wraps navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}> 
+        <div className={`${styles.boxWidth}}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}}`}>
+          Hero         
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}}`}>
+        <div className={`${styles.boxWidth}}`}>
+          Stats        
+          Business
+          Billing 
+          CardDeal
+          Clients 
+          CTA 
+          Footer
+        </div>
+      </div>
+
+
     </div>
   );
 
