732025cce34ccbdd942402dfab3b2f03bb28436d
Ewa <ewni@startupcentral.dk>
2022-12-01 16:19:02 +0100

    adding smth

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,24 @@ const App = () => (
     {/* div below wraps navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}> 
         <div className={`${styles.boxWidth}}`}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}}`}>
-          Hero         
+          <Hero/>         
         </div>
       </div>
-      <div className={`bg-primary ${styles.flexStart}}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}}`}>
         <div className={`${styles.boxWidth}}`}>
-          Stats        
-          Business
-          Billing 
-          CardDeal
-          Clients 
-          CTA 
-          Footer
+          <Stats/>        
+          <Business/>
+          <Billing/> 
+          <CardDeal/>
+          <Clients/> 
+          <CTA/> 
+          <Footer/>
         </div>
       </div>
 
