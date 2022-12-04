2d870c488db78770aa04b4fd957063bb4afc28d6
EwaNikodem <ewanikodem44@gmail.com>
2022-12-04 15:05:28 +0100

    implementing navbar and fixing position

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 //inside this App.jsx, we import every component, that is implemented separately one by one
-import styles from './style';
+import styles from "./style";
 
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 const App = () => (
     //this div wraps whole application
     <div className="bg-primary w-full overflow-hidden">  
     {/* div below wraps navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}> 
-        <div className={`${styles.boxWidth}}`}>
-          <Navbar/>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero/>         
         </div>
       </div>
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}}`}>
-        <div className={`${styles.boxWidth}}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
           <Stats/>        
           <Business/>
           <Billing/> 
