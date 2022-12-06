56fa1206530dcf114164137b23ab7ce0a42e953d
Ewa <ewni@startupcentral.dk>
2022-12-06 16:29:24 +0100

    init work for stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { stats } from '../constants';
+import  styles from '../style';
 
-function Stats() {
-  return (
-    <div>Stats</div>
+
+const Stats = () =>(
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat) => (
+        <div key={stat.id} className>
+          <h4 className=''>
+            {stat.value}</h4>
+            <p>{stat.title}</p>
+        </div>
+      ))}
+    </section>
   )
-}
+
 
 export default Stats
